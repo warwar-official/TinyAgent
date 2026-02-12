5b7fb6aeaba9dbad5bc9046b036acb89dfab64a3
warwar-official <somethingstrenge@gmail.com>
2026-02-12 22:21:32 +0200

    weather tool update

---
+++ imports/tools/get_current_weather.py
@@ -0,0 +1,21 @@
+from requests import request
+
+def get_current_weather(location: str) -> dict:
+    location_url = location.replace(" ", "%20")
+    location_json = request("GET", f"https://nominatim.openstreetmap.org/search?q={location_url}&format=json").json()
+    if location_json:
+        lat = location_json[0]["lat"]
+        lon = location_json[0]["lon"]
+        weather_json = request("GET", f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true").json()
+        if weather_json:
+            answer = {
+                "temperature": weather_json["current_weather"]["temperature"],
+                "windspeed": weather_json["current_weather"]["windspeed"],
+                "winddirection": weather_json["current_weather"]["winddirection"],
+                "weathercode": weather_json["current_weather"]["weathercode"],
+                "is_day": weather_json["current_weather"]["is_day"],
+                "time": weather_json["current_weather"]["time"],
+            }
+            return answer
+        return {"error": "Weather not found"}
+    return {"error": "Location not found"}
