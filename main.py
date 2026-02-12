e299abca270295789bdfb64f03d4532527f7c45a
warwar-official <somethingstrenge@gmail.com>
2026-02-12 02:24:15 +0200

    loop update

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from imports.models.gemini import GeminiModel
 from imports.tools.get_current_temperature import get_current_temperature
 import json
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 tool_table = {
     "get_current_temperature":get_current_temperature
@@ -19,7 +23,6 @@ Avaible tools:
 'description':'Gets the current temperature for a given location.',
 'parameters':
     {
-        'type':'object',
         'properties': {
             'location':{
                 'type':'string',
@@ -30,41 +33,46 @@ Avaible tools:
     }
 """
 
-def get_toolcall(message: str) -> str:
-    toolcall = message[message.find("{"):message.rfind("}")]
-    return toolcall
-
-def main():
-    """model = GeminiModel("gemma-3-27b-it", "")
+def history_to_payload(history: list[tuple[str, str]]) -> dict:
     payload = {
         'contents': [
-            {
-                'role':'user',
-                'parts': [
-                    {
-                        'text': sys_prompt
-                    }
-                ]
-            },
-            {
-                'role':'user',
-                'parts': [
-                    {
-                        'text':'What the wether in Kyiv?'
-                    }
-                ]
-            }
         ]
     }
+    for role, text in history:    
+        payload['contents'].append({
+            'role': role,
+            'parts': [
+                {
+                    'text': text
+                }
+            ]
+        })
+    return payload
+
+def perform_loop(model: GeminiModel, prompt: str):
+    history: list[tuple[str, str]] = []
+    history.append(("user", sys_prompt))
+    history.append(("user", prompt))
+    payload = history_to_payload(history)
     answer = model.make_request(payload)
-    tool = get_toolcall(answer)"""
-    tool = '{"toolcall": {"name":"get_current_temperature", "arguments": {"location": "Kyiv"}}}'
-    tool_json = json.loads(tool)["toolcall"]
-    func = globals()[tool_json["name"]]
-    res = func(**tool_json["arguments"])
-    print(res)
-    """print(answer)
-    print(tool)"""
+    print(answer)
+    history.append(("model", answer))
+    tool = parse_toolcall(answer)
+    res = tool_table[tool["name"]](**tool["arguments"])
+    history.append(("user", f"Tool {tool['name']}, arguments: {tool['arguments']}, returned result: {res}"))
+    payload = history_to_payload(history)
+    answer = model.make_request(payload)
+    history.append(("model", answer))
+    return answer
+
+def parse_toolcall(message: str) -> dict:
+    toolcall = message[message.find("{"):message.rfind("}")+1]
+    return json.loads(toolcall)
+
+def main():
+    model = GeminiModel("gemma-3-27b-it", os.getenv("GEMINI_API_KEY"))
+    answer = perform_loop(model, "What the wether in Kyiv?")
+    print(answer)
 
 if __name__ == "__main__":
     main()
